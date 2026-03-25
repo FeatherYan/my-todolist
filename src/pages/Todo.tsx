7ee173b64235cabf6325ff75aaa8894239f2fea9
FeatherYan <544752400@qq.com>
2026-03-25 19:01:49 +0800

    feat: implement basic todo add and delete

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -1,3 +1,31 @@
+import { useState } from "react"
 export default function Todo() {
-  return <div>Todo Page</div>
+    const [todos, setTodos] = useState<string[]>([]);
+    const [inputValue, setInputValue] = useState("");
+    const addTodo = () => {
+        if(inputValue.trim() !== "") {
+            setTodos([...todos, inputValue]);
+            setInputValue("");
+        }
+    }
+    const delTodo = (index: number) => {
+        setTodos(todos.filter((_, i) => i !== index));
+    }
+    return (
+        <div>
+            <h1>Todos</h1>
+            <div>
+                <input
+                    type="text"
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
+                    placeholder="Enter a new todo"
+                />
+                <button onClick={addTodo}>Add Todo</button>
+            </div>
+            <ul>
+                {todos.map((item, index) => (<li key={index}>{item} <button onClick={() => delTodo(index)}>Delete</button></li>))}
+            </ul>        
+        </div>
+    )
 }
